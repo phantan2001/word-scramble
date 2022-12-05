8665595b08632bd5a22d8aef9d6081c648188423
Phan Tan <pttan1304@gmail.com>
2022-12-05 22:23:38 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,72 @@
+const wordText = document.querySelector(".word");
+let hintClick = document.querySelector(".hint");
+const hintText = document.querySelector(".solution span");
+const refreshBtn = document.querySelector(".refresh-word");
+const checkwordBtn = document.querySelector(".check-word");
+let inputField = document.getElementById("enter-word");
+let scoreEl = document.querySelector(".score span");
+let timeText = document.querySelector(".time b");
+
+let correctWord, timer;
+
+
+const initTimer = maxTime => {
+    clearInterval(timer);
+    timer = setInterval(() => {
+        if(maxTime > 0) {
+            maxTime--;
+            timeText.innerText = maxTime;
+        }
+        if(maxTime <= 10) {
+            timeText.style.color = 'red';
+        }
+        if(maxTime === 0) {
+            clearInterval(timer);
+            alert(`Time off! ${correctWord.toUpperCase()} was the correct word`);
+            initGame();
+        }
+    }, 1000)
+}
+
+const initGame = () => {
+    initTimer(30);
+    let randomObj  = words[Math.floor(Math.random() * words.length)];
+    let wordArray =  randomObj.word.split("");
+
+    for(let i = wordArray.length - 1; i > 0; i--){
+        let j = Math.floor(Math.random() * (i + 1));
+        [wordArray[i], wordArray[j]] = [wordArray[j], wordArray[i]]
+    }
+
+    hintClick.addEventListener("click", function() {
+        hintText.innerText = randomObj.hint;
+    })
+    wordText.innerText = wordArray.join("");
+    correctWord = randomObj.word.toLowerCase();
+    inputField.value = "";
+    inputField.setAttribute("maxlength", correctWord.length);
+    console.log(randomObj);
+}
+
+initGame();
+let score = 0;
+scoreEl.innerText = score;
+const checkWord = () => {
+    let userWord = inputField.value.toLocaleLowerCase();
+    if(!userWord) return alert("Please enter a word check");
+    if(userWord !== correctWord) {
+        return alert(`Oops! ${userWord} is not a correct word`);
+    } else {
+        alert(`Congratulations! ${userWord.toUpperCase()} is a correct word`);
+        
+        scoreEl.innerText = ++score;
+        initGame();
+    }
+}
+
+refreshBtn.addEventListener("click", function () {
+    initGame();
+})
+checkwordBtn.addEventListener("click", function() {
+    checkWord();
+})
---
+++ js/words.js
@@ -0,0 +1,163 @@
+const words = [
+    {
+        word: "addition",
+        hint: "The process of adding numbers"
+    },
+    {
+        word: "meeting",
+        hint: "Event in which people come together"
+    },
+    {
+        word: "number",
+        hint: "Math symbol used for counting"
+    },
+    {
+        word: "government",
+        hint: "the most important of a country"
+    },
+    {
+        word: "map",
+        hint: "used to find the way"
+    },
+    {
+        word: "computer",
+        hint: "electronic equipment that controls information and data"
+    },
+    {
+        word: "knowledge",
+        hint: "related to education"
+    },
+    {
+        word: "industry",
+        hint: "part of the economy"
+    },
+    {
+        word: "freedom",
+        hint: "anyone wants"
+    },
+    {
+        word: "discussion",
+        hint: "Teamwork really needs this"
+    },
+    {
+        word: "inflation",
+        hint: "lose value of money"
+    },
+    {
+        word: "responsibility",
+        hint: "High position with high ..."
+    },
+    {
+        word: "studio",
+        hint: "support for audio recording work"
+    },
+    {
+        word: "percentage",
+        hint: "used a lot in math"
+    },
+    {
+        word: "patience",
+        hint: "It's a trait that needs training"
+    },
+    {
+        word: "secretary",
+        hint: "support for director or president"
+    },
+    {
+        word: "psychology",
+        hint: "This is a course about people"
+    },
+    {
+        word: "storage",
+        hint: "used to save important things"
+    },
+    {
+        word: "exchange",
+        hint: "the act of trading"
+    },
+    {
+        word: "canvas",
+        hint: "peace of fabric for oil painting"
+    },
+    {
+        word: "garden",
+        hint: "space for planting flower and plant"
+    },
+    {
+        word: "position",
+        hint: "Location of someone or something"
+    },
+    {
+        word: "pocket",
+        hint: "a bag for carrying small items"
+    },
+    {
+        word: "needle",
+        hint: "A thin and sharp metal pin"
+    },
+    {
+        word: "expert",
+        hint: "person with extensive knowledge"
+    },
+    {
+        word: "statement",
+        hint: "A declaration of something"
+    },
+    {
+        word: "second",
+        hint: "One-sixtieth of a minute"
+    },
+    {
+        word: "library",
+        hint: "Place containing collection of books"
+    },
+    {
+        word: "group",
+        hint: "A number of objects or persons"
+    },
+    {
+        word: "country",
+        hint: "A politically identified region"
+    },
+    {
+        word: "expansion",
+        hint: "The process of increase or grow"
+    },
+    {
+        word: "tongue",
+        hint: "The muscular organ of mouth"
+    },
+    {
+        word: "comfort",
+        hint: "A pleasant feeling of relaxation"
+    },
+    {
+        word: "feather",
+        hint: "Hair like outer covering of bird"
+    },
+    {
+        word: "alcohol",
+        hint: "used in parties, eating"
+    },
+    {
+        word: "cancer",
+        hint: "a very dangerous disease for humans"
+    },
+    {
+        word: "mixture",
+        hint: "mix many substances together"
+    },
+    {
+        word: "protection",
+        hint: "action to ensure safety"
+    },
+    {
+        word: "atmosphere",
+        hint: "sustain human life"
+    },
+    {
+        word: "negotiation",
+        hint: "the act of two or more parties exchanging and discussing"
+    },
+    
+] 
